d41c4c5cf9ccbc23db425cb2ac2845e7b41bcdf9
Allen Koren <theplatapi@gmail.com>
2025-12-19 11:47:53 -0800

    Bump dep range

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
     install_requires=[
-        "graphql-core>=3.2,<3.3",
+        "graphql-core>=3.2",
         "typing-extensions>=4.1,<5; python_version < '3.8'",
     ],
     python_requires=">=3.6,<4",
